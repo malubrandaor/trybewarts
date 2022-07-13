46542cb104ddd75dc10c16a1e6a2960d7880f0a3
malubrandaor <malubrandaor@gmail.com>
2022-07-13 20:10:14 -0300

    requisito18

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const email = document.getElementById('email');
 const senha = document.getElementById('password');
 const botao = document.getElementById('buttonlogin');
+const enviar = document.getElementById('submit-btn');
+const checkBox = document.getElementById('agreement');
+
 
 function validation() {
   if (email.value === 'tryber@teste.com' && senha.value === '123456') {
@@ -10,3 +13,13 @@ function validation() {
   }
 }
 botao.addEventListener('click', validation);
+
+
+function confirmation(){
+    if (enviar.hasAttribute('disabled') === true ){
+        enviar.removeAttribute ('disabled');
+    } else {
+        enviar.setAttribute('disabled', 'disabled');
+    }
+}
+checkBox.addEventListener('click', confirmation);
